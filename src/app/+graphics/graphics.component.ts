6ed404dd5cc55c85d0b852948ad09f07822f32ff
Carey <evcarey@qualcomm.com>
2017-02-27 18:18:51 -0800

    Rendering correctly

--- src/app/+graphics/graphics.component.ts
+++ src/app/+graphics/graphics.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'ec-graphics',
-    templateUrl: 'graphics.component.html'
+    templateUrl: 'graphics.component.html',
+    styleUrls: ['graphics.component.scss']
 })
 export class GraphicsComponent implements OnInit {
     constructor() { }
--- src/app/+graphics/modifier/modifier.component.ts
+++ src/app/+graphics/modifier/modifier.component.ts
@@ -1,5 +1,6 @@
 import { RendererService } from './../renderer/renderer.service';
 import { Component, OnInit, Input } from '@angular/core';
+import { Mesh, PointLight, AmbientLight, Geometry, BoxGeometry, Material, MeshPhongMaterial, Vector3 } from "three";
 
 @Component({
     selector: 'modifier',
@@ -7,6 +8,10 @@ import { Component, OnInit, Input } from '@angular/core';
     // templateUrl: 'modifier.component.html'
 })
 export class ModifierComponent implements OnInit {
+
+    cube: Mesh;
+    light: PointLight;
+
     constructor(private renderer: RendererService) { }
 
     @Input()
@@ -15,5 +20,25 @@ export class ModifierComponent implements OnInit {
     }
 
     ngOnInit() {
+        let pLight = new PointLight(0xffffff, .7);
+        pLight.position.set(3, 10, 3);
+        this.renderer.scene.add(pLight);
+
+        let aLight = new AmbientLight(0xffffff, 0.1);
+        this.renderer.scene.add(aLight);
+
+        let geometry: Geometry = new BoxGeometry(2, 2, 2);
+        let material: Material = new MeshPhongMaterial({
+            color: 0x00ff00,
+            specular: 0xffffff,
+            shininess: 56
+        });
+        this.cube = new Mesh(geometry, material);
+        this.cube.translateZ(-5);
+        this.cube.translateY(-1);
+        this.cube.rotateY(0.1);
+        this.renderer.scene.add(this.cube);
+
+
     }
 }
