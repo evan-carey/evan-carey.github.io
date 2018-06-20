42212a92feef7d23b7b4d6db8bcec4ebef4df14f
Evan Carey <evanacarey@gmail.com>
2018-06-19 20:57:16 -0700

    launch config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
-import { mat } from '@angular/material'
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PageNotFoundComponent } from './page-not-found.component';
@@ -10,17 +9,10 @@ import { ProjectsService } from './projects/projects.service';
 
 import { CoreModule } from './core/core.module';
 
-
 @NgModule({
-    imports: [
-        BrowserModule,
-        HttpModule,
-        AppRoutingModule,
-        CoreModule,
-        CommonModule
-    ],
+    imports: [BrowserModule, HttpModule, AppRoutingModule, CoreModule, CommonModule],
     declarations: [AppComponent, PageNotFoundComponent],
     providers: [ProjectsService],
     bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
